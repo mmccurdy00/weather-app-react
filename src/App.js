c5cb54d0fb971e875cb0661494057de341299236
mmccurdy00 <142942959+mmccurdy00@users.noreply.github.com>
2023-10-14 18:31:55 -0500

    Added CSS

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ function App() {
     <div className="App">
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">
+        <button class="btn btn-outline-dark" data-bs-theme="dark" type="submit">
           ?
         </button>
       </form>
       <h1>PARIS</h1>
       <h2>Tuesday 2:34 PM</h2>
-      <img src="#" alt="weather icon" />
-      <h1>8°C</h1>
+      <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+        alt="weather icon"
+      />
+      <h1 class="temp">8°C</h1>
       <hr />
       <div class="container text-center">
         <div class="row">
-          <div class="col">Humidity: <br/> 28%</div>
-          <div class="col">Feels Like: <br/> 19°C</div>
-          <div class="col">Wind: <br/> 12 mph</div>
+          <div class="col break-line">
+            Humidity: <br /> 28%
+          </div>
+          <div class="col break-line">
+            Feels Like: <br /> 19°C
+          </div>
+          <div class="col">
+            Wind: <br /> 12 mph
+          </div>
         </div>
       </div>
     </div>
