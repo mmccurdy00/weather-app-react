298dc8d2c23f4ff0a2eff1ef712f17c0722db291
mmccurdy00 <142942959+mmccurdy00@users.noreply.github.com>
2023-10-14 17:51:49 -0500

    Added basic HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">
+          ?
+        </button>
+      </form>
+      <h1>PARIS</h1>
+      <h2>Tuesday 2:34 PM</h2>
+      <img src="#" alt="weather icon" />
+      <h1>8°C</h1>
+      <hr />
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">Humidity: <br/> 28%</div>
+          <div class="col">Feels Like: <br/> 19°C</div>
+          <div class="col">Wind: <br/> 12 mph</div>
+        </div>
+      </div>
     </div>
   );
 }
